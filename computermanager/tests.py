892cec247d7020b1ce17e30cb3a8f6a7b02c76b1
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2018-02-19 18:11:13 -0500

    Test for Computer Status
    A test that goes through all Computer choices and makes sure they can be saved and loaded

--- computermanager/tests.py
+++ computermanager/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
+
+from .models import Computer
+
+class ComputerModelTest(TestCase):
+
+    def test_all_possible_status(self):
+
+        testjsondata = {'computer': 'test', 'status': 'Waiting'}
+        for choice in Computer.CHOICES:
+            testsave = Computer(name=testjsondata['computer'] + choice[1], status=choice[1], start_time=timezone.now())
+            testsave.save()
+            testload = Computer.objects.get(name=testjsondata['computer'] + choice[1])
