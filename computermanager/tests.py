59e6c38bd2c0d904f64036d8b55d2c887dc63347
DukeOfNewYork <DukeOfNewYork@users.noreply.github.com>
2018-02-19 19:53:39 -0500

    Update tests.py

--- computermanager/tests.py
+++ computermanager/tests.py
@@ -1,7 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils import timezone
-
-# Create your tests here.
 
 from .models import Computer
 
@@ -9,8 +8,14 @@ class ComputerModelTest(TestCase):
 
     def test_all_possible_status(self):
 
-        testjsondata = {'computer': 'test', 'status': 'Waiting'}
+        # First a test to make sure full_clean is properly checking the choices field
+        testjsondata = {'computer': 'test', 'status': 'BadInput'}
+        testsave = Computer(name=testjsondata['computer'], status=testjsondata['status'], start_time=timezone.now())
+        with self.assertRaises(ValidationError):
+            testsave.full_clean()
+
+        # Checks that each choice can be saved and loaded
         for choice in Computer.CHOICES:
             testsave = Computer(name=testjsondata['computer'] + choice[1], status=choice[1], start_time=timezone.now())
             testsave.save()
-            testload = Computer.objects.get(name=testjsondata['computer'] + choice[1])
+            self.assertIs(Computer.objects.get(name=testjsondata['computer'] + choice[1]).status == choice[1], True)
